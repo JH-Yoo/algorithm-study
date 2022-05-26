45e4552a3464164ff34c72315e0f11085a1ceefb
유주현 <gus6359@gmail.com>
2022-05-27 03:50:06 +0900

    논문 번역을 위한 개행 문자 정리 알고리즘

---
+++ Etc/trimText/trimText.js
@@ -0,0 +1,31 @@
+// 논문 번역하다가 /n때문에 불편해서 직접 만든 코드
+
+const fs = require('fs')
+
+const trimText = (text) => {
+  return text
+    // 단어 이어붙이는 경우
+    .replace(/\-\r\n+/g, '')
+    // 문단 시작인 경우 개행시킴
+    .replace(/.\r\n([A-Z])/g, '\n$1')
+    // 보통 개행 문자
+    .replace(/\r\n+/g, ' ')
+}
+
+const readFile = (path) => {
+  const file = fs.readFileSync(path, 'utf8')
+  return file
+}
+
+const writeFile = (path, data) => {
+  fs.writeFileSync(path, data, 'utf8')
+}
+
+const excute = () => {
+  const text = readFile('./data/input.txt')
+  // console.log(text)
+  console.log(trimText(text))
+  writeFile('./data/output.txt', trimText(text))
+}
+
+excute()
